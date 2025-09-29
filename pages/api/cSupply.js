e2ac0dea8568ba3a426392cab588887195577a50
Kazim Akgul <kazimakgul@gmail.com>
2025-09-29 11:35:25 +0300

    fixed

--- pages/api/cSupply.js
+++ pages/api/cSupply.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import BigNumber from 'bignumber.js';
 
-const apiKey = 'R7BR3EI9DR9AQB9BWTU3MIYEC3KFWB8EUV';
+const apiKey = process.env.ETHERSCAN_API_KEY;
 const contractAddress = '0x56633733fc8BAf9f730AD2b6b9956Ae22c6d4148';
 const decimals = 18; // replace this with the number of decimal places your token uses
-const nonCirculatingSupply = new BigNumber(125).multipliedBy(new BigNumber(10).pow(6)); // 125 million
 const apiBaseUrl = 'https://api.etherscan.io/v2/api';
 const chainId = 137; // Polygon mainnet
 
 export default async (req, res) => {
     try {
+        if (!apiKey) {
+            throw new Error('Missing ETHERSCAN_API_KEY environment variable');
+        }
+
         const response = await axios.get(apiBaseUrl, {
             params: {
                 chainid: chainId,
@@ -28,8 +31,7 @@ export default async (req, res) => {
 
         const totalSupplyInWei = new BigNumber(result);
         const totalSupply = totalSupplyInWei.dividedBy(new BigNumber(10).pow(decimals));
-        const circulatingSupply = totalSupply.minus(nonCirculatingSupply); // Subtracting non-circulating supply
-        res.status(200).send(circulatingSupply.toString(10)); // Only sending the circulating supply value as a string
+        res.status(200).send(totalSupply.toString(10));
     } catch (error) {
         console.error(error);
         res.status(500).send('Error fetching circulating supply.');
